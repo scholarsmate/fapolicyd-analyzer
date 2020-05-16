8750222929fc7c11c738cee37d2c77ada5e1480f
Davin Shearer <davin.shearer@evc.ctc.com>
2020-05-16 08:56:13 -0400

    introduce sqlite3

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h> 
+#include <sqlite3.h>
 	
 int main(int argc, char * argv[])
 {
-	printf("Hello world!\n");
+	printf("SQLite library version: %s\n", sqlite3_libversion()); 
 	return 0;
 }
 

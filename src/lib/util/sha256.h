6e2edc77b69e3dcc2d1038f8c40859633d767b0a
Davin Shearer <davin.shearer@evc.ctc.com>
2020-06-15 18:01:17 -0400

    rid ourselves of gcrypt

--- src/lib/util/sha256.h
+++ src/lib/util/sha256.h
@@ -7,20 +7,32 @@
 extern "C" {
 #endif
 
-#define SHA256_DIGEST_SIZE 32            // SHA256 outputs a 32 byte digest
+#define SHA256_DIGEST_SIZE 32 // SHA256 outputs a 32 byte digest
 
 typedef struct sha256_ctx_struct sha256_ctx_t;
 
-sha256_ctx_t * sha256_create();
-void sha256_destroy(sha256_ctx_t * ctx);
+sha256_ctx_t *sha256_create();
+void sha256_destroy(sha256_ctx_t *ctx);
 
-void sha256_update(sha256_ctx_t * ctx, const unsigned char * data, size_t len);
-void sha256_final(sha256_ctx_t * ctx, unsigned char * digest);  /* digest must be at least 32-bytes in length */
+void sha256_update(sha256_ctx_t *ctx, const unsigned char *data, size_t len);
+void sha256_final(
+    sha256_ctx_t *ctx,
+    unsigned char *digest); /* digest must be at least 32-bytes in length */
 
-int sha256_file(char * hash_buf, size_t hash_buf_len, const char * path);
+/**
+ * @brief Computes a SHA256 hash of the contents of the file pointed to by the
+ * given path
+ *
+ * @param hash_buf character buffer to store the hexidecimal representation of
+ * the hash
+ * @param hash_buf_len the size of the character buffer
+ * @param path path to the file whose contents will be hashed
+ * @return int returns zero (OK) on success and non-zero otherwise
+ */
+int sha256_file(char *hash_buf, size_t hash_buf_len, const char *path);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif   // SHA256_H
+#endif // SHA256_H
